8536dbb11b820606f44d3f973b258fe4ec5a99d1
Jmorozova2019 <Jannamoroz10@ya.ru>
2019-10-07 11:17:23 +0300

    Rename method testAutorisation() to test_GetSessionID(), delete unused rows

--- src/test/java/API_ARM.java
+++ src/test/java/API_ARM.java
@@ -1,23 +1,21 @@
-import io.restassured.http.Cookie;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import static java.util.Objects.isNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.annotations.Test;
 
-import java.net.URI;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-
 public class API_ARM {
-    private static final String BASE_URL = "http://213.128.208.33:8080";
+    private static final String BASE_URI = "http://213.128.208.33:8080";
 
+    /**
+     * Получение ID сессии (аутентификация)
+     */
     @Test
-    public void testAutorisation(){
+    public void test_GetSessionID(){
         Map<String, Object> paramsReqMap = new HashMap<>();
         paramsReqMap.put("success", "/share/page/");
         paramsReqMap.put("failure", "/share/page/?error=true");
@@ -25,28 +23,21 @@ public class API_ARM {
         paramsReqMap.put("password", "12345");
 
         Response response = null;
-        try{
+        try {
             response =
                     given()
-                    .params(paramsReqMap)
-                    .post(BASE_URL)
+                            .params(paramsReqMap)
+                    .post(BASE_URI)
                     .then().statusCode(200).extract().response();
-        }catch(java.lang.AssertionError e){
+        } catch(java.lang.AssertionError e){
             System.out.println("Autorisation: "+ e.getMessage());
         }
-        System.out.println("StatusCode: "+ response.getStatusCode());
 
-        Headers headerResponse = response.getHeaders();
-        //почему-то возвращает только один набор Set-Cookie
-        //headerResponse.forEach(h ->  System.out.println(h.getName()));
-        //List<String> listSetCookies= headerResponse.getValues("Set-Cookie");
-        //System.out.println(listSetCookies.size());
-        //listSetCookies.forEach(u -> System.out.println(u));
-
-        Map<String, String> allCookies = response.getCookies();
-        System.out.println("----------Cookies--------------");
-        allCookies.forEach((a, b) ->  System.out.println(a + ": " + b));
-
-        return;
-    }
+        if (isNull(response)) {
+            System.out.println("Response is null, sessionId not found ");
+            return;
+        }
+        String sessionId = response.getSessionId();
+        System.out.println(sessionId);
+     }
 }
